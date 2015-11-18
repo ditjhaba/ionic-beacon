2d4d3ddadb82a5472f447175c4ab33d063789ce6
Ditjhaba Selemela <edselemela@gmail.com>
2015-11-18 14:49:46 +0200

    Adding functionality to fetch all beacons within the proximity.

---
+++ www/js/controllers.js
@@ -0,0 +1,25 @@
+angular.module('ionic.beacon', [])
+    .controller("ionic.beacon", ['$scope', '$rootScope', '$ionicPlatform', '$cordovaBeacon'],
+    function ($scope, $rootScope, $ionicPlatform, $cordovaBeacon) {
+
+        $scope.beacons = {};
+
+        $ionicPlatform.ready(function () {
+            $cordovaBeacon.requestWhenInUseAuthorization();
+
+            $rootScope.$scope.$on('$cordovaBeacon:didRangeBeaconsInRegion', function (event, pluginResult) {
+                var uniqueBeaconKey;
+
+                for (var i = 0; i < pluginResult.beacons.length; i++) {
+                    uniqueBeaconKey = pluginResult.beacons[i].uuid + ":" + pluginResult.beacons[i].major + ":" +
+                        pluginResult.beacons[i].minor;
+                    $scope.beacons[uniqueBeaconKey] = pluginResult.beacons[i];
+                }
+
+                $scope.$apply();
+            });
+
+            $cordovaBeacon.startRangingBeaconsInRegion(
+                $cordovaBeacon.createBeaconRegion("testing", "61687109-905F-4436-91F8-E602F514C96D"));
+        });
+    });
